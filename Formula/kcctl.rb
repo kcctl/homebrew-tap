1ea255b1aa32cda50b1e6bb19ba154e3da7fe090
jreleaserbot <jreleaser@kordamp.org>
2021-12-22 14:13:03 +0000

    kcctl v1.0.0.Alpha5

--- Formula/kcctl.rb
+++ Formula/kcctl.rb
@@ -1,16 +1,16 @@
 class Kcctl < Formula
   desc "kcctl -- A CLI for Apache Kafka Connect"
   homepage "https://github.com/kcctl/kcctl"
-  version "1.0.0.Alpha3"
+  version "1.0.0.Alpha5"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Alpha3/kcctl-1.0.0.Alpha3-linux-x86_64.zip"
-    sha256 "ba8b641c2693330b351412361583b2b3fd6890ef0cee25fba8bb82ea4e510e6a"
+    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Alpha5/kcctl-1.0.0.Alpha5-linux-x86_64.zip"
+    sha256 "669006978af4599da25221fbeaabf6cd956061238a3a1ab452435f0b574d16a8"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Alpha3/kcctl-1.0.0.Alpha3-osx-x86_64.zip"
-    sha256 "525a87b3b0e91c6fc6af82f2e6f4238e207b9793add7e1577272e7747a1546fb"
+    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Alpha5/kcctl-1.0.0.Alpha5-osx-x86_64.zip"
+    sha256 "562e59dd6890567492eb4f43a2dc64c45caf78d7f7794039a1a179c0e7af9fbb"
   end
 
 
@@ -21,6 +21,6 @@ class Kcctl < Formula
 
   test do
     output = shell_output("#{bin}/kcctl --version")
-    assert_match "1.0.0.Alpha3", output
+    assert_match "1.0.0.Alpha5", output
   end
 end
