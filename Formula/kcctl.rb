b58ebe232ae2809bcbe4da8af30d63a9d4472533
jreleaserbot <jreleaser@kordamp.org>
2023-01-22 08:25:09 +0000

    kcctl v1.0.0.Beta3

--- Formula/kcctl.rb
+++ Formula/kcctl.rb
@@ -1,27 +1,31 @@
+# Generated with JReleaser 1.4.0 at 2023-01-22T08:24:53.248069676Z
 class Kcctl < Formula
   desc "kcctl -- A CLI for Apache Kafka Connect"
   homepage "https://github.com/kcctl/kcctl"
-  version "1.0.0.Alpha5"
+  version "1.0.0.Beta3"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Alpha5/kcctl-1.0.0.Alpha5-linux-x86_64.zip"
-    sha256 "669006978af4599da25221fbeaabf6cd956061238a3a1ab452435f0b574d16a8"
-  elsif OS.mac?
-    # TODO: Check for Rosetta if not running on an Intel CPU?
-    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Alpha5/kcctl-1.0.0.Alpha5-osx-x86_64.zip"
-    sha256 "562e59dd6890567492eb4f43a2dc64c45caf78d7f7794039a1a179c0e7af9fbb"
-  else
-    raise RuntimeError.new("kcctl is only supported on Linux (with Intel CPU) or MacOS")
+    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Beta3/kcctl-1.0.0.Beta3-linux-x86_64.zip"
+    sha256 "2a44837c59bec175a183ff458f8fe1bdb69de6dbc1cd72bae507c0709ce00e5b"
   end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Beta3/kcctl-1.0.0.Beta3-osx-x86_64.zip"
+    sha256 "a1702acdc3bf1ae442690d51cbc6339158625f218054d57d999cdd033b0247a4"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/kcctl/kcctl/releases/download/v1.0.0.Beta3/kcctl-1.0.0.Beta3-osx-x86_64.zip"
+    sha256 "a1702acdc3bf1ae442690d51cbc6339158625f218054d57d999cdd033b0247a4"
+  end
+
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/kcctl"
+    bin.install_symlink "#{libexec}/bin/kcctl" => "kcctl"
   end
 
   test do
     output = shell_output("#{bin}/kcctl --version")
-    assert_match "1.0.0.Alpha5", output
+    assert_match "1.0.0.Beta3", output
   end
 end
